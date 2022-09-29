488dd22d7a8e47afa00d204955e8c88485e1573c
Aleksandra Kameneva <kameneva_fkp@mail.ru>
2022-09-29 23:56:36 +0500

    ДЗ 7

---
+++ src/js/Gui.js
@@ -0,0 +1,81 @@
+/* eslint-disable class-methods-use-this */
+export default class Gui {
+  constructor() {
+    this.widget = document.querySelector('.widget');
+    this.tasksList = document.querySelector('.tasks_list');
+    this.popUp = document.querySelector('.popup');
+    this.btnCancel = document.querySelector('.cancel');
+    this.btnOk = document.querySelector('.okeyBtn');
+  }
+
+  createTask(id, status, name, date) {
+    let done = '';
+    if (status) {
+      done += '<svg class="done" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h48v48H0z" fill="none"/><path d="M36 14l-2.83-2.83-12.68 12.69 2.83 2.83L36 14zm8.49-2.83L23.31 32.34 14.97 24l-2.83 2.83L23.31 38l24-24-2.82-2.83zM.83 26.83L12 38l2.83-2.83L3.66 24 .83 26.83z"/></svg>';
+    }
+    const newTask = `
+    <tr class="task" data-id="${id}">
+        <td class="status" data-id="${status}">
+            <p class="round">${done}</p>
+        </td>
+        <td class="text" data-id="name">${name}</td>
+        <td class="date" data-id="date">${date}</td>
+        <td class="change">
+            <p class="round edit-btn"><svg class="img edit" data-id="edit" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M6 34.5v7.5h7.5l22.13-22.13-7.5-7.5-22.13 22.13zm35.41-20.41c.78-.78.78-2.05 0-2.83l-4.67-4.67c-.78-.78-2.05-.78-2.83 0l-3.66 3.66 7.5 7.5 3.66-3.66z"/><path d="M0 0h48v48h-48z" fill="none"/></svg></p>
+        </td>
+        <td class="change">
+            <p class="round remove-btn"><svg class="img close" data-id="remove" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z"/><path d="M0 0h48v48h-48z" fill="none"/></svg></p>
+        </td>
+    </tr>`;
+    return newTask;
+  }
+
+  addPopUp(header, title, description, idform, name, id) {
+    const newPopUp = `
+      <div class="content">
+          <h3 class="popover_header">${header}</h3>
+          <form class="form" name="${name}" data-id="${id}" data-idform="${idform}" enctype="multipart/form-data">
+              <p class="name-new-ticket">Краткое описание</p>
+              <input class="input-name-new-ticket" name="title" data-id="name-edit" value="${title}">
+              <p class="description-new-ticket">Подробное описание</p>
+              <textarea class="input-description-new-ticket" name="description" data-id="description-edit">${description}</textarea>
+              <p class="btn_block">
+                  <a class="btn cancel">Отмена</a>
+                  <a class="btn okeyBtn">ОК</a>
+              </p>
+          </form>
+      </div>`;
+    return newPopUp;
+  }
+
+  showDescription(header, name, description) {
+    const newPopUp = `
+    <div class="content">
+        <h3 class="popover_header">${header}</h3>
+          <p class="name-new-ticket">Краткое описание</p>
+          <p class="show-description" data-id="name-edit">${name}</p>
+          <p class="description-new-ticket">Подробное описание</p>
+          <p class="show-description" data-id="description-edit">${description}</p>
+          <p class="btn_block">
+              <a class="btn cancel">Закрыть</a>
+          </p>
+    </div>
+    `;
+    return newPopUp;
+  }
+
+  showVertification(name, idform) {
+    const newPopUp = `
+    <div class="content">
+        <h3 class="popover_header">Вы хотите удалить тикет <span class="show-name">${name}</span>?</h3>
+        <form class="form" action="submit" data-idform="${idform}">
+          <p class="btn_block">
+              <a class="btn cancel">Нет</a>
+              <a class="btn okeyBtn">Да</a>
+          </p>
+        </form>
+    </div>
+    `;
+    return newPopUp;
+  }
+}
---
+++ src/js/Logic.js
@@ -0,0 +1,159 @@
+export default class Logic {
+  constructor(gui) {
+    this.gui = gui;
+    this.tickets = null;
+    // this.url = 'http://localhost:7070/';
+    this.url = 'https://alexkam-ahj-http-server.herokuapp.com';
+    this.popUpReset = this.popUpReset.bind(this);
+    this.popUpSubmit = this.popUpSubmit.bind(this);
+    this.remove = this.remove.bind(this);
+  }
+
+  init() {
+    this.getTickets();
+
+    this.gui.widget.addEventListener('click', (e) => {
+      e.preventDefault();
+      let currId;
+      if (e.target.tagName === 'path' && e.target.closest('.img')) {
+        currId = e.target.closest('.img').dataset.id;
+      } else {
+        currId = e.target.dataset.id;
+      }
+
+      if (currId === 'edit') {
+        this.editTicket(e);
+      } else if (currId === 'remove') {
+        this.removeTicket(e);
+      } else if (currId === 'name') {
+        this.showDescription(e);
+      } else if (currId === 'addTicket') {
+        this.addTicket(e);
+      }
+    });
+  }
+
+  async sendXHR(method, query, type) {
+    const xhr = new XMLHttpRequest();
+    if (method === 'GET') {
+      const url = `${this.url}?method=${query}`;
+      xhr.open(method, url, false);
+      xhr.send();
+    } else if (method === 'POST') {
+      const url = `${this.url}?method=${type}`;
+      xhr.open(method, url, true);
+      xhr.send(query);
+    } else if (method === 'DELETE') {
+      const url = `${this.url}?method=deleteTicket&id=${query}`;
+      xhr.open(method, url, false);
+      xhr.send();
+    }
+    return (xhr.responseText);
+  }
+
+  async getTickets() {
+    const result = JSON.parse(await this.sendXHR('GET', 'allTickets'));
+    this.tickets = result;
+    this.fillFields((this.tickets));
+  }
+
+  async popUpSubmit(e) {
+    e.preventDefault();
+    const form = e.target.closest('.form');
+    const name = form.querySelector('.input-name-new-ticket').value;
+    const description = form.querySelector('.input-description-new-ticket').value;
+    const idForm = form.dataset.idform;
+    if (idForm === 'edit') {
+      const { id } = form.dataset;
+      const request = new FormData();
+      request.append('id', id);
+      request.append('title', name);
+      request.append('description', description);
+      await (this.sendXHR('POST', request, 'editTicket'));
+    } else if (idForm === 'add') {
+      const request = new FormData();
+      request.append('title', name);
+      request.append('description', description);
+      await (this.sendXHR('POST', request, 'createTicket'));
+    }
+    this.getTickets();
+    await this.gui.popUp.classList.add('hidden');
+  }
+
+  async remove(e) {
+    e.preventDefault();
+    const form = e.target.closest('.form');
+    const { id } = form.dataset;
+    const request = new FormData();
+    request.append('id', id);
+    await (this.sendXHR('DELETE', request, ''));
+    await this.gui.popUp.classList.add('hidden');
+    this.getTickets();
+  }
+
+  popUpReset(e) {
+    e.preventDefault();
+    this.gui.popUp.classList.add('hidden');
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async editTicket(e) {
+    e.preventDefault();
+    this.gui.popUp.innerHTML = '';
+    const task = e.target.closest('.task');
+    const ticket = JSON.parse(await this.sendXHR('GET', `ticketById&id=${task.dataset.id}`));
+    this.gui.popUp.classList.remove('hidden');
+    this.gui.popUp.innerHTML += this.gui.addPopUp('Редактировать тикет', ticket.name, ticket.description, 'edit', ticket.name, ticket.id);
+    const submit = this.gui.popUp.querySelector('.okeyBtn');
+    const cancel = this.gui.popUp.querySelector('.cancel');
+    submit.addEventListener('click', this.popUpSubmit);
+    cancel.addEventListener('click', this.popUpReset);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async removeTicket(e) {
+    e.preventDefault();
+    this.gui.popUp.innerHTML = '';
+    const task = e.target.closest('.task');
+    const ticket = JSON.parse(await this.sendXHR('GET', `ticketById&id=${task.dataset.id}`));
+    this.gui.popUp.classList.remove('hidden');
+    this.gui.popUp.innerHTML += this.gui.showVertification(ticket.name, 'remove');
+    const submit = this.gui.popUp.querySelector('.okeyBtn');
+    const cancel = this.gui.popUp.querySelector('.cancel');
+    submit.addEventListener('click', this.remove);
+    cancel.addEventListener('click', this.popUpReset);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async showDescription(e) {
+    e.preventDefault();
+    this.gui.popUp.innerHTML = '';
+    const task = e.target.closest('.task');
+    const ticket = JSON.parse(await this.sendXHR('GET', `ticketById&id=${task.dataset.id}`));
+    this.gui.popUp.classList.remove('hidden');
+    this.gui.popUp.innerHTML += this.gui.showDescription('Тикет', ticket.name, ticket.description);
+    const cancel = this.gui.popUp.querySelector('.cancel');
+    cancel.addEventListener('click', this.popUpReset);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  addTicket(e) {
+    e.preventDefault();
+    this.gui.popUp.innerHTML = '';
+    this.gui.popUp.classList.remove('hidden');
+    this.gui.popUp.innerHTML += this.gui.addPopUp('Добавить тикет', '', '', 'add', '');
+    const submit = this.gui.popUp.querySelector('.okeyBtn');
+    const cancel = this.gui.popUp.querySelector('.cancel');
+    submit.addEventListener('click', this.popUpSubmit);
+    cancel.addEventListener('click', this.popUpReset);
+  }
+
+  fillFields(tArr) {
+    this.gui.tasksList.innerHTML = '';
+    tArr.forEach((ticket) => {
+      this.gui.tasksList.innerHTML += this.gui.createTask(
+        ticket.id, ticket.status, ticket.name, ticket.created,
+      );
+    });
+  }
+}
